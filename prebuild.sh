c05a0aa3341ef9754d9e714a0340e3f8ceb50e75
ivan1001 <782740456@qq.com>
2024-05-25 18:12:15 +0800

    google test

--- prebuild.sh
+++ prebuild.sh
@@ -6,6 +6,30 @@ export PATH=${PREBUILT_DIR}:$PATH
 nproc=2
 
 
+function build_gflags()
+{
+    cd ${CURRENT_DIR}/gflags && cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${PREBUILT_DIR} -B build && cmake --build build -j ${nproc} && cmake --build build --target install 
+
+    if [ $? -ne 0 ];then
+        echo "build gflags failed"
+        exit 
+    fi
+
+    echo "BUILD gflags COMPLETE"
+}
+
+function build_glogs()
+{
+    cd ${CURRENT_DIR}/glog && cmake -S . -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_GFLAGS=OFF -DCMAKE_INSTALL_PREFIX=${PREBUILT_DIR} -B build && cmake --build build -j ${nproc} && cmake --build build --target install
+
+    if [ $? -ne 0 ];then
+        echo "build glog failed"
+        exit 
+    fi
+
+    echo "BUILD glog COMPLETE" 
+}
+
 function build_protobuf()
 {
     # git submodule update --init --recursive
@@ -13,16 +37,18 @@ function build_protobuf()
     cd ${CURRENT_DIR}/protobuf && cmake -G "Unix Makefiles"  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${PROTOBUF_INSTALL_PATH} -B build && cmake --build build -j ${nproc} && cmake --build build --target install
 
     if [ $? -ne 0 ]; then
-	    echo "build tutorial failed"
+	    echo "build protobuf failed"
 	    exit
     fi
     
-    echo "BUILD tutorial COMPLETED"
+    echo "BUILD protobuf COMPLETED"
 }
 
 function do_build()
 {
-    build_protobuf
+    # build_gflags
+    build_glogs
+    # build_protobuf
 }
 
 function do_clean()
