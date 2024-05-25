691f49bfc8c08a4a4d95773f652ef9e229584fda
ivan1001 <782740456@qq.com>
2024-05-25 14:47:33 +0800

    ff

--- prebuild.sh
+++ prebuild.sh
@@ -8,5 +8,30 @@ nproc=2
 
 function build_protobuf()
 {
-  cd ${CURRENT_DIR}/protobuf
+    # git submodule update --init --recursive
+    PROTOBUF_INSTALL_PATH=${PREBUILT_DIR}/protobuf
+    cd ${CURRENT_DIR}/protobuf && cmake -G "Unix Makefiles"  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=${PROTOBUF_INSTALL_PATH} -B build && cmake --build build -j ${nproc} && cmake --build build --target install
+
+    if [ $? -ne 0 ]; then
+	    echo "build tutorial failed"
+	    exit
+    fi
+    
+    echo "BUILD tutorial COMPLETED"
 }
+
+function do_build()
+{
+    build_protobuf
+}
+
+function do_clean()
+{
+    echo "current do nothing"
+}
+
+if [[ $1 == "clean" ]]; then
+    do_clean
+else
+    do_build
+fi
